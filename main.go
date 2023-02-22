ce305c120477e3b497b19d6fff6af3a424050fe0
Aidan Matchette <aidan.j.matchette@gmail.com>
2023-02-22 11:09:08 -0800

    Completed part two of solution
    Used go routines, select, channels, and timer to handle letting the quiz
    game be timed.

--- main.go
+++ main.go
@@ -6,57 +6,76 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
-    csvFilename := flag.String("csv", "problems.txt", "a csv file in the format of 'question,answer'")
-    flag.Parse()
+	csvFilename := flag.String("csv",
+		"problems.csv",
+		"a csv file in the format of 'question,answer'")
+	timeLimit := flag.Int("limit", 30, "The time limit for the quiz in seconds")
 
-    file, err := os.Open(*csvFilename)
-    if err != nil {
-        exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFilename))
-    }
-    r := csv.NewReader(file)
+	flag.Parse()
 
-    lines, err := r.ReadAll()
-    if err != nil {
-        exit("Failed to parse the provided CSV file")
-    }
-    problems := parseLines(lines)
+	file, err := os.Open(*csvFilename)
 
-    correct := 0
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFilename))
+	}
+	r := csv.NewReader(file)
 
-    for i, p := range problems {
-        fmt.Printf("Question #%d: %s = \n", i+1, p.question)
-        var answer string
-        fmt.Scanf("%s\n", &answer)
-        if answer == p.answer {
-            correct++
-        }
-    }
-    fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse the provided CSV file")
+	}
+	problems := parseLines(lines)
+
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+	correct := 0
+
+    problemLoop:
+	for i, p := range problems {
+        fmt.Printf("Question #%d: %s = ", i+1, p.question)
+        answerCh := make(chan string)
+
+        go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+            answerCh <- answer
+        }()
+
+		select {
+		case <-timer.C:
+			fmt.Println()
+            break problemLoop
+        case answer := <-answerCh:
+			if answer == p.answer {
+				correct++
+			}
+		}
+	}
+	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
 }
 
 func parseLines(lines [][]string) []problem {
-    ret := make([]problem, len(lines))
+	ret := make([]problem, len(lines))
 
-    for i, line := range lines {
-        ret[i] = problem{
-            question: line[0],
-            answer: strings.TrimSpace(line[1]),
-        }
-    }
-    return ret
+	for i, line := range lines {
+		ret[i] = problem{
+			question: line[0],
+			answer:   strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
 
 }
 
 type problem struct {
-    question string
-    answer string
+	question string
+	answer   string
 }
 
 func exit(msg string) {
-    fmt.Println(msg)
-    os.Exit(1)
+	fmt.Println(msg)
+	os.Exit(1)
 }
-
